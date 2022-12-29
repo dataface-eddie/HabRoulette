6de7735361459114752bbbd2703b7f145f6b8f28
Eddie Young <77517856+dataface-eddie@users.noreply.github.com>
2022-12-29 16:57:16 -0600

    OG commint

---
+++ Schema_Build/Stored_Procedures/habit_complete.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE PROCEDURE habit_complete(user_completed BOOL)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO habit.fact_habit_event
+    SELECT id
+        , habit_name
+        , minutes_needed
+        , score
+        , user_completed::BOOL AS completed
+        , insert_time
+    FROM habit.fact_habit_event_stg;
+    
+    UPDATE habit.fact_habit_event
+    SET score = 0
+    WHERE completed IS FALSE;
+
+    TRUNCATE habit.fact_habit_event_stg;
+END;$$
+;
+
