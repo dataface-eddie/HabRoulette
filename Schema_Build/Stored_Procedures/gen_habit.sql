9a6ca62cff6179af3dbc0ce728dae720ec54830d
Eddie Young <77517856+dataface-eddie@users.noreply.github.com>
2022-12-29 15:16:42 -0600

    MY FIRST STORED PROCEDURE!

---
+++ Schema_Build/Stored_Procedures/gen_habit.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE PROCEDURE gen_habit(minutes_available INT)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+--clear the staging table
+    TRUNCATE habit.fact_habit_event_stg;
+--load the staging table so we can display the randomly chosen habit and the user
+--can then decide if they want to complete it
+    INSERT INTO habit.fact_habit_event_stg
+    SELECT id
+        , habit_name
+        , minutes_needed
+        , score 
+        , NOW() AS insert_time
+    FROM habit.v_habit_generator
+    WHERE minutes_needed <= minutes_available
+    ORDER BY RANDOM()
+    LIMIT 1;
+    
+end; $$
