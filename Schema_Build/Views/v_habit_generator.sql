3478f527f6dadc7a61b2155382c0209cb875ed71
Eddie Young <77517856+dataface-eddie@users.noreply.github.com>
2023-01-01 08:55:25 -0600

    adding category

--- Schema_Build/Views/v_habit_generator.sql
+++ Schema_Build/Views/v_habit_generator.sql
@@ -1,12 +1,15 @@
-CREATE VIEW habit.v_habit_generator
+CREATE OR REPLACE VIEW habit.v_habit_generator
 AS
 SELECT dh.id
     , dh.habit_name
     , dhta.minutes_needed
     , dhs.score
+    , category
 FROM habit.dim_habit AS dh
 JOIN habit.dim_habit_time_allocation AS dhta
     ON dh.id = dhta.id
 LEFT JOIN habit.dim_habit_score AS dhs
     ON dh.id = dhs.id
+LEFT JOIN habit.dim_habit_category AS dhc
+    ON dh.id = dhc.id
 ;
